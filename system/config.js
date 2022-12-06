ecd64950d83cd7bd2311806d6c543182a9aa9f31
AzwanXD <105867685+Azwanxd123455@users.noreply.github.com>
2022-12-06 13:44:53 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '6285887776722'
+global.owner = '6281210334790'
 // Owner name
-global.owner_name = 'Wildan Izzudin'
+global.owner_name = 'azwan syahidan'
 // Database name (Default: database)
 global.database = 'database'
 // Maximum upload file size limit (Default : 100 MB)
@@ -28,7 +28,7 @@ global.timezone = 'Asia/Jakarta'
 // Bot version
 global.version = '2.2.2',
 // Bot name
-global.botname = `© neoxr-bot v${global.version} (Public Bot)`
+global.botname = `© md-bot v${global.version} (Public Bot)`
 // Footer text
 global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
 // Global status
